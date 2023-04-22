73b62c168ae9688265b33d67ec79a309ad980c7a
K1NG <77655302+King12689@users.noreply.github.com>
2023-04-21 20:08:05 -0400

    Create twrp_CPH1717.mk

---
+++ twrp_CPH1717.mk
@@ -0,0 +1,22 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Virtual A/B OTA
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+# Inherit from star device
+$(call inherit-product, A71/Oppo/CPH1717/device.mk)
+
+# Inherit some common recovery stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := CPH1717
+PRODUCT_NAME := twrp_CPH1717
+PRODUCT_BRAND := Oppo
+PRODUCT_MODEL := A71
+PRODUCT_MANUFACTURER := Oppo
