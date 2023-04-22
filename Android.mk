2544fda8f1acff93b16a634f59a24b14e9092a00
K1NG <77655302+King12689@users.noreply.github.com>
2023-04-21 21:06:07 -0400

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,4 +1,10 @@
-# path to device
+# Replace $$DEVICE$$ with your Device Name's Value. Mine is Primo_RX5.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value, Mine is WALTON 
 
-LOCAL_PATH := device/oppo/CPH1717
+ifneq ($(filter CPH1717,$(TARGET_DEVICE)),)
 
+LOCAL_PATH := device/Oppo/CPH1717
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
+
+endif
