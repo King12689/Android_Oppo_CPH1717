39998b92c8f9c7086650e47a2a9f5df0af54a0a6
K1NG <77655302+King12689@users.noreply.github.com>
2023-04-21 20:00:11 -0400

    Add files via upload

---
+++ omniCPH1717.mk
@@ -0,0 +1,30 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Add this line if your device is 64-bit
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+# Otherwise, If you have 32-bit device, add the below line instead of above line
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk
+
+# Another common config inclusion
+$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# If you are building from OmniROM's minimal source, Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Replace $$DEVICE$$ with your Device Name's Value.
+# Replace $$BRAND$$ with your Brand's / Manufacturer's Value.
+PRODUCT_COPY_FILES += device/Oppo/CPH1717/prebuilt/zImage:kernel
+# Fles under $(LOCAL_PATH)/recovery/root/ gets automatically copied into recovery
+# PRODUCT_COPY_FILES += $(LOCAL_PATH)/recovery/root/*:root/*
+
+PRODUCT_DEVICE := CPH1717
+PRODUCT_NAME := omni_CPH1717
+PRODUCT_BRAND := oppo
+PRODUCT_MODEL := A71
+PRODUCT_MANUFACTURER := Oppo
+
+# Forcefully add mtp support (adb is already there)
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp
+
+
